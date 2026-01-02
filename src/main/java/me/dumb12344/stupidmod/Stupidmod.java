570985e2f81cf8b9d0db64f5f6a325a4dcf99f0c
dumb12344 <167947276+dumb12344@users.noreply.github.com>
2026-01-02 15:31:11 -0800

    Nuke

--- src/main/java/me/dumb12344/stupidmod/Stupidmod.java
+++ src/main/java/me/dumb12344/stupidmod/Stupidmod.java
@@ -3,6 +3,8 @@ package me.dumb12344.stupidmod;
 import com.mojang.logging.LogUtils;
 import me.dumb12344.stupidmod.bedrockitems.registry.BedrockItemRegistry;
 import me.dumb12344.stupidmod.bedrockitems.registry.BedrockEntityTypeRegistry;
+import me.dumb12344.stupidmod.nuke.NukeRegistry;
+import me.dumb12344.stupidmod.nuke.NukeRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.client.renderer.entity.ThrownItemRenderer;
 import net.minecraft.core.registries.Registries;
@@ -34,7 +36,7 @@ public class Stupidmod {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "stupidmod";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    //private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "stupidmod" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     // Create a Deferred Register to hold Items which will all be registered under the "stupidmod" namespace
@@ -57,7 +59,7 @@ public class Stupidmod {
     }).build());*/
 
     //Funi sound
-    public static final RegistryObject<SoundEvent> FUNISOUND = SOUND_EVENTS.register("sounds_damage", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID,"sounds_damage")));;
+    public static final RegistryObject<SoundEvent> FUNISOUND = SOUND_EVENTS.register("sounds_damage", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID,"sounds_damage")));
     public Stupidmod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
@@ -72,6 +74,9 @@ public class Stupidmod {
         CREATIVE_MODE_TABS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so sounds get registered
         SOUND_EVENTS.register(modEventBus);
+        NukeRegistry.BLOCKS.register(modEventBus);
+        NukeRegistry.ENTITIES.register(modEventBus);
+        NukeRegistry.ITEMS.register(modEventBus);
         BedrockEntityTypeRegistry.ENTITY_TYPES.register(modEventBus);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -116,6 +121,7 @@ public class Stupidmod {
             EntityRenderers.register(BedrockEntityTypeRegistry.BEDROCK_PICKAXE_PROJECTILE.get(), ThrownItemRenderer::new);
             EntityRenderers.register(BedrockEntityTypeRegistry.BEDROCK_SWORD_PROJECTILE.get(), ThrownItemRenderer::new);
             EntityRenderers.register(BedrockEntityTypeRegistry.BEDROCK_ENDER_PEARL_PROJECTILE.get(), ThrownItemRenderer::new);
+            EntityRenderers.register(NukeRegistry.PRIMED_NUKE.get(), NukeRenderer::new);
             // Some client setup code
             //LOGGER.info("HELLO FROM CLIENT SETUP");
             //LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
